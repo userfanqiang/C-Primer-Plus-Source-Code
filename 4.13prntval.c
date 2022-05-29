d74c83bc370c3697954308f890ae7cfc1b1b4834
userfanqiang <fanqiangemail@qq.com>
2022-05-29 11:15:27 +0800

    5.29

---
+++ 4.13prntval.c
@@ -0,0 +1,12 @@
+/* prntval.c--pritf()的返回值 */
+
+#include <stdio.h>
+
+int main()
+{
+    int bph20 = 212;
+    int rv;
+    rv = printf("%d F is water's boiling point.\n", bph20);
+    printf("The printf() function printed %d characters.\n", rv);
+    return 0;
+}
--- 4.8floats.c
+++ 4.8floats.c
@@ -9,10 +9,10 @@ int main(void)
     printf("*%e*\n", RENT);
     printf("*%4.2f*\n", RENT);
     printf("*%3.1f*\n", RENT);
-    printf("*%10.31f*\n", RENT);
-    printf("*10.3E*\n", RENT);
+    printf("*%10.3f*\n", RENT);
+    printf("*%10.3E*\n", RENT);
     printf("*%+4.2f*\n", RENT);
-    printf("*010.2f*\n", RENT);
+    printf("*%010.2f*\n", RENT);
 
     return 0;
 }
