d5488214d5fa1546ac4adc0701f7789c8a4b915d
userfanqiang <fanqiangemail@qq.com>
2022-05-29 16:08:12 +0800

    5.29

---
+++ 5.3golf.c
@@ -0,0 +1,12 @@
+/* golf.c--高尔夫竞标赛计分卡 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int jean, tarzan, cheeta;
+    cheeta = tarzan = jean = 68;
+    printf("                     cheeta    tarzan    jean\n");
+    printf("Fitst round score %4d %8d %8d\n", cheeta, tarzan, jean);
+    return 0;
+}
